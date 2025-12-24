a6996a5e3fd1c7ae17bdea53c0b1ba116914bc3c
Ahmed Ali Abdullah <a7medali725@gmail.com>
2025-12-24 22:36:04 +0200

    add login endpoint

---
+++ Talapat.Api/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities.Identity;
+using Talapat.Api.DTOs;
+using Talapat.Api.Errors;
+
+namespace Talapat.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+            return Ok(new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "FakeToken"
+
+            });
+
+
+        }
+    }
+}
---
+++ Talapat.Api/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Talapat.Api.DTOs
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Token { get; set; } = null!;
+    }
+}
