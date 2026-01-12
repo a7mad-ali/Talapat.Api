4c311159eeb3e666ea959559e835fe56538a67bd
Ahmed Ali Abdullah <a7medali725@gmail.com>
2026-01-12 12:00:08 +0200

    adding the Order aggreation entities

---
+++ Talabat.Core/Entities/Order Aggregate/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+    public class Order : BaseEntity
+    {
+        public string BuyerEmail { get; set; } = null!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus status { get; set; } = OrderStatus.Pending;
+        public Address ShippingAddress { get; set; } = null!;
+         //   public int DeliveryMethodId {  get; set; }//foregin key
+        public DeliveryMethod DeliveryMethod { get; set; } = null!; // navigational property one 
+
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public decimal SubTotal { get; set; }
+        //[NotMapped]
+        //public decimal Total =>  SubTotal + DeliveryMethod.Cost;  
+        public string PaymentIntentId { get; set; } = string.Empty;
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+
+    }
+} 
